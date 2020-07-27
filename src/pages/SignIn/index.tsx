a6999b8613dbe29c143f3d2b4cd682d98d6fea59
Lucas Silveira <lugano1995@gmail.com>
2020-07-27 19:43:17 -0300

    feat: add logo image

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Image } from 'react-native';
+
+import logotipo from '../../assets/img/logo.png';
 
 import { Container } from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logotipo} />
+    </Container>
+  );
 };
 
 export default SignIn;
