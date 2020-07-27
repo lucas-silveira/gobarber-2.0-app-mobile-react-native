367a3a8bb0c7ff3e84197b6880df5752c47afade
Lucas Silveira <lugano1995@gmail.com>
2020-07-27 19:19:34 -0300

    feat: create signIn and signUp pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 // import { Container } from './styles';
 
-const App: React.FC = () => <View />;
+const App: React.FC = () => (
+  <>
+    <StatusBar backgroundColor="#312e38" barStyle="light-content" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}></View>
+  </>
+);
 
 export default App;
